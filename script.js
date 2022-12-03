58d3c74200ebed31f18e0cf497e85b4989432d86
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-12-03 22:58:47 +0530

    ex 06.1
    Implementing getNextDate function

--- script.js
+++ script.js
@@ -59,6 +59,42 @@ function reverseStr(str) {
     }
     return flag;
   }
+  function getNextDate(date) {
+    var day = date.day + 1;//incrementing the day
+    var month = date.month;
+    var year = date.year;
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (month == 2) {//check for leap year,feb month
+      if (isLeapYear(year)) {
+        if (day > 29) {
+          day = 1;
+          month++;
+        }
+      }
+      else {
+        if (day > 28) {//normal condition
+          day = 1;
+          month++;
+        }
+      }
+    }
+    else {
+      if (day > daysInMonth[month - 1]) { //-1,since indexing starting from 0,so adjusting.
+        day = 1;
+        month++;
+      }//month changes,hence increasing.
+    }
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+    return {
+      day: day,
+      month: month,
+      year: year
+    };
+  }
+  
   var date = {
     day: 2,
     month: 11,
@@ -66,3 +102,4 @@ function reverseStr(str) {
   };
   // console.log(getAllDateFormats(date))
   console.log(checkPalindromeForAllDAteFormats(date));
+  console.log(getNextDate(date));
