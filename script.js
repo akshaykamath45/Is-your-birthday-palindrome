e6e7190e1b123dce4cd2805e38ee51b484c9bd90
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-12-03 20:52:33 +0530

    ex 02
    function to check  palindrome

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function reverseStr(str) {
     var listOfChars = str.split('');//['h','e','l','l','o']
     var reverseListOfChars = listOfChars.reverse();
-    var reversedStr = reverseListOfChars.join();
+    var reversedStr = reverseListOfChars.join('' );
     return reversedStr;
     // return str.split('').reverse().join();
   }
-  console.log(reverseStr("Hello"));
+  console.log(reverseStr("mom"));
+
+  function isPalindrome(str){
+        var reverse=reverseStr(str);
+        // if(str === reverse){
+        //     return true;
+        // }
+        // else{ 
+        //     return false
+        // }
+        return str === reverse;
+  }
+
+  console.log(isPalindrome("242"));
+  console.log(isPalindrome('oppo'));
+  console.log(isPalindrome('racecar'));
+  console.log(isPalindrome('mom'));
+  console.log(isPalindrome('momo'));
